8953ea95ed81f7d4ac14f7f575a00e11a55a3095
Oatmeal88 <84576350+Oatmeal88@users.noreply.github.com>
2022-09-13 12:09:02 -0400

    Initial Commit

---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+#include <nds.h>
+
+#include <stdio.h>
+
+int main()
+{
+    consoleDemoInit();
+
+    irqInit();
+
+    irqEnable(IRQ_VBLANK);
+
+    while (true)
+    {
+        printf("\nQueen Elizabeth II\n1926 - 2022\n\nThis IS Nitro Emulator Unit has\ncurrently been disabled due to\nthe Royal Period of Mourning andwill not be able to be used\nuntil Monday 19 September\n\nFor more information please\nvisit:\n\nhttp://www.warioworld.com/nitro/royal-mourning/");
+
+    swiWaitForVBlank();
+    consoleClear();
+    }
+return 0;
+}
